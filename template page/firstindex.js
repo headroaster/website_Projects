65bea9465f722264ae7dae60b24931217e65471b
headroaster <headroaster@headroaster.com>
2015-03-20 02:56:41 -0700

    -

---
+++ template page/firstindex.js
@@ -0,0 +1,74 @@
+$(document).ready(function() {
+
+    $("head").append("<title>Buckets of Sickness</title>");
+    $("#main").append("<div class='corner', id='c1'><p>corner c1</p></div>");
+
+    $("#main").append("<div class='headline', id='headline'><p>headline</p></div>");
+
+    $("#main").append("<div class='corner', id='c2'><p>corner c2</p></div>");
+
+    $("#main").append("<div class='barber', id='b1'><p>barber b1</p></div>");
+
+    $("#main").append("<div class='center', id='center'><p>center</p></div>");
+
+    $("#main").append("<div class='barber', id='b2'><p>barber b2</p></div>");
+
+    $("#main").append("<div class='footer', id='footer'><p>footer</p></div>");
+
+    $("#footer").append("<a href='index.html'><p>CLICKS</p></a>");
+
+/*$("#c1").position({
+
+        my: "upper left",
+        at: "upper  left",
+        of: "body"
+    });
+
+    $("#headline").position({
+
+        my: "center top",
+        at: "center top",
+        of: "#main"
+    });
+
+    $("#c2").position({
+
+        my: "upper  right",
+        at: "upper  right",
+        of: "#main"
+    });
+
+    $("#b1").position({
+
+        my: "left center",
+        at: "left center",
+        of: "#main"
+    });
+
+    $("#center").position({
+
+        my: "center",
+        at: "center bottom",
+        of: "#main"
+    });
+
+
+    $("#b2").position({
+
+        my: "right center",
+        at: "right center",
+        of: "#main"
+    });
+
+
+    $("#footer").position({
+
+        my: "center bottom",
+        at: "center bottom",
+        of: " #center"
+    });*/
+
+
+
+
+});
